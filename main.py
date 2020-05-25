de63244712e65cb8936bdf32569c87511b1ac63c
strapbool <rafael_rx@hotmail.com>
2020-05-25 18:21:18 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# Imports the Google Cloud client library
+from google.cloud import storage
+
+
+class Google:
+    def __init__(self):
+        # Instanciar cliente
+        self.storage_client = storage.Client()
+
+    def create_new_bucket(self):
+        # Setar nome do bucket
+        # O nome do bucket é global e precisa ser um nome que nunca foi utilizado
+        bucket_name = "suridatateste"
+        # Criar novo bucket
+        bucket = self.storage_client.create_bucket(bucket_name)
+
+    def upload_file(self, bucket_name, source_file_name):
+        # Selecionar Bucket
+        bucket = self.storage_client.bucket(bucket_name)
+        # Permitir disponibilizar objetos de dados, chamados blobs
+        blob = bucket.blob(source_file_name)
+        # Realizar upload
+        blob.upload_from_filename(source_file_name)
+
+    def list_files(self, bucket_name):
+        # Listar arquivos do bucker
+        blobs = self.storage_client.list_blobs(bucket_name)
+
+        # Percorrer blobs
+        for blob in blobs:
+            print(blob.name)
+
+
+google = Google()
+# google.create_new_bucket()
+# google.upload_file("suridatateste", "teste.txt")
+# google.list_files("suridatateste")
